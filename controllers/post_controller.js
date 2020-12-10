301aabcfec432a1a5564e5b591765dfa04a06d2b
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2020-12-10 18:57:27 -0500

    mistake

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -144,7 +144,7 @@ async function likePost(parentData) {
 
   if(liked == '1') {
     console.log('getting into remove like func')
-    const docArr = await poparentst.collection('likes').where('userID','==', userID).get();
+    const docArr = await parent.collection('likes').where('userID','==', userID).get();
     if (docArr.size != 1) {
       console.log('ERROR: should like once');
       return null;
