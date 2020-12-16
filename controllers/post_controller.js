f55290deeb3570df289d6fd81d635b313a5a7b80
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2020-12-16 17:20:20 -0500

    parint line 2

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -112,7 +112,8 @@ async function editPost(postData) { // remember dot notation (content.title) for
 
 // Delete post
 async function remove(parentData) {
-  console.log('parentData', parentData)
+  console.log('parentData1', parentData.query)
+  console.log('parentData2', parentData.body)
   const id = parentData.query.id;
   const type = parentData.query.type;
   const parent = db.collection(type).doc(id);
