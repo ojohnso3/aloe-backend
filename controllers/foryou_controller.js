c9116190f64ce5ebae7510ccebeafdefa3a1f8ab
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2020-12-17 22:26:54 -0500

    just int

--- controllers/foryou_controller.js
+++ controllers/foryou_controller.js
@@ -145,12 +145,12 @@ async function getSurveyResults(answerData) {
   const answerDoc = await answer.get()
   if(!answerDoc.exists) {
     console.log("error")
-    return {results: -1}
+    return -1
   }
 
   const answerUsers = await answer.collection('users').get();
   console.log("size", answerUsers.size)
-  return {results: answerUsers.size}
+  return answerUsers.size
 }
 // async function getSurveyResults(promptData) {
 //   console.log('sd', promptData.body)
