28d0bbd68cd2f13f3125481a0880884d4423f4af
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2021-01-13 18:24:47 -0500

    more

--- functions/src/processing.js
+++ functions/src/processing.js
@@ -22,13 +22,13 @@ function postProcessing(post) {
   console.log("try guys", postData)
   console.log("anon", postData.anonymous)
 
-
   const anonymous = true;
   if (postData.anonymous == '0') {
+    console.log("check 0", postData.anonymous)
     anonymous = false;
   }
 
-  console.log('PIG 1.2 postdata', anonymous)
+  console.log('oop', anonymous)
 
   const ret = {
     userID: postData.userid,
