98d2a3400f4db7c27b5b477131598f020dd3330f
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2021-01-14 11:13:25 -0500

    writing resource script

--- functions/src/controllers/resource_controller.js
+++ functions/src/controllers/resource_controller.js
@@ -1,6 +1,7 @@
 const db = require('../firebase/db.js');
 const middleware = require('../middleware.js');
-const helpers = require('../helpers.js');
+// const helpers = require('../helpers.js');
+const csvtojson = require("csvtojson");
 
 // - Get Resources
 async function getResources(type) {
@@ -12,6 +13,23 @@ async function getResources(type) {
   }
   return {results: resources.docs.map((doc) => middleware.resourceMiddleware(doc.id, doc.data()))};
 }
+
+async function parseCSV(csvData) {
+  for(let i=0; i<csvData.length; i++) {
+    let memberProfile = csvData[i]
+    let name;
+  }
+}
+
+async function addResources() {
+  csvtojson()
+    .fromFile("Member-Typeform-Grid view.csv")
+    .then(csvData => {
+      createProfile(csvData)
+    })
+}
+
+
 
 
 module.exports = {
