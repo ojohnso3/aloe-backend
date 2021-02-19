123b94a63065df308644beb99d8dc0c47e314dfd
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2021-02-19 15:46:30 -0500

    making prod only for now

--- functions/src/firebase/admin.js
+++ functions/src/firebase/admin.js
@@ -1,7 +1,8 @@
 const admin = require('firebase-admin');
 
-console.log('db check', process.env.PRODUCTION)
-const serviceAccount = require(process.env.PRODUCTION ? './production-key.json' : './dev-key.json');
+// console.log('db check', process.env.PRODUCTION);
+// const serviceAccount = require(process.env.PRODUCTION ? './production-key.json' : './dev-key.json');
+const serviceAccount = require('./production-key.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
