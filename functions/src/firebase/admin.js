aa0f1ba7dc8d0383e8026a79fa7bf10caa73ae8f
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2021-02-19 12:41:08 -0500

    added .env + checking db connection

--- functions/src/firebase/admin.js
+++ functions/src/firebase/admin.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
 
+console.log('db check', process.env.PRODUCTION)
 const serviceAccount = require(process.env.PRODUCTION ? './production-key.json' : './dev-key.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
