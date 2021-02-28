a5a0a05b750c12aea6227d7e6a4f6d34a45de013
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2021-02-28 16:02:03 -0500

    testing test db w app

--- functions/src/firebase/admin.js
+++ functions/src/firebase/admin.js
@@ -3,10 +3,10 @@ const admin = require('firebase-admin');
 // console.log('db check', process.env.PRODUCTION);
 
 // 1) For running locally
-// const serviceAccount = require(process.env.PRODUCTION ? './production-key.json' : './dev-key.json');
+const serviceAccount = require(process.env.PRODUCTION ? './production-key.json' : './dev-key.json');
 
 // 2) For deploying for app
-const serviceAccount = require('./production-key.json');
+// const serviceAccount = require('./production-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
