f0e5763877729814808cea6955bd06b78565906f
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2021-03-01 21:01:41 -0500

    back to testing

--- functions/src/firebase/admin.js
+++ functions/src/firebase/admin.js
@@ -3,13 +3,13 @@ const admin = require('firebase-admin');
 // console.log('db check', process.env.PRODUCTION);
 
 // 1) For running locally
-const serviceAccount = require(process.env.PRODUCTION ? './production-key.json' : './dev-key.json');
-process.env.PRODUCTION ? console.log('YOU ARE ON PRODUCTION') : console.log('YOU ARE ON TESTING');
+// const serviceAccount = require(process.env.PRODUCTION ? './production-key.json' : './dev-key.json');
+// process.env.PRODUCTION ? console.log('YOU ARE ON PRODUCTION') : console.log('YOU ARE ON TESTING');
 
 
 // 2) For deploying for app
-// const serviceAccount = require('./production-key.json');
-// console.log('YOU ARE ON PRODUCTION')
+const serviceAccount = require('./production-key.json');
+console.log('YOU ARE ON PRODUCTION');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
